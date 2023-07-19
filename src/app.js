1eb12caff81b192aff9fb096f0b1b8a5286248eb
Miguel Angel Martinez Pazos <92331020+MPMiguel@users.noreply.github.com>
2023-07-19 21:35:34 +0000

    works

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const pronouns = ["the", "our", "their", "his", ""];
+const adjectives = ["great", "big", "small", "loud", "mean", "pro"];
+const nouns = ["runner", "racoon", "coffee", "tea", "shop", "cup", ""];
+const extensions = [".com", ".es", ".net", ".edu", ".org"];
+
+const generateDomainNames = () => {
+  let generatedDomains = [];
+
+  pronouns.forEach(pronoun => {
+    adjectives.forEach(adjective => {
+      nouns.forEach(noun => {
+        extensions.forEach(extension => {
+          let domain = pronoun + adjective + noun + extension;
+          generatedDomains.push(domain);
+        });
+      });
+    });
+  });
+
+  return generatedDomains;
+};
+
+const displayDomainNames = () => {
+  const generatedDomains = generateDomainNames();
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = generatedDomains.join("<br>");
+};
+
+const generateButton = document.getElementById("generateButton");
+generateButton.addEventListener("click", displayDomainNames);
